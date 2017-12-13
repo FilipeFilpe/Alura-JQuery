6fa750d8c0e7b8f8b2aa1eaa21acab91f174f0bc
filipe <filipe@localhost.localdomain>
2017-12-13 00:27:44 -0200

    Aula dois

---
+++ alura-typer/public/js/main.js
@@ -0,0 +1,18 @@
+var frase  = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+// evento
+campo.on("input", function(){
+    // val dar valor de campos de inputs
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+})
